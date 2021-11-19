ed9186c6e0c565d507ec32e7c9a1861ac2e11901
Arthur Piccoli <>
2021-11-19 17:53:40 -0300

    create MyHomePage class

--- flutter_counter_example/lib/main.dart
+++ flutter_counter_example/lib/main.dart
@@ -8,9 +8,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Text('Flutter Demo Home Page'),
-      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(title),
     );
   }
 }
