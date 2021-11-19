c59b9f82d2b2f2c8c821bdc3f42f56ce87cef766
Arthur Piccoli <>
2021-11-19 18:44:19 -0300

    convert MyHomePage from stateless to stateful

--- flutter_counter_example/lib/main.dart
+++ flutter_counter_example/lib/main.dart
@@ -18,17 +18,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   final int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
