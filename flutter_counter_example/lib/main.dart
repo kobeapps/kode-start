c5ef9e7b4893cf478f6650f8dbaa87a7a375a787
Arthur Piccoli <>
2021-11-19 17:54:08 -0300

    add app title

--- flutter_counter_example/lib/main.dart
+++ flutter_counter_example/lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
