3d51bc8f4220768b08a32bd91daa7a0119044318
Arthur Piccoli <>
2021-11-19 17:58:02 -0300

    hide debug banner

--- flutter_counter_example/lib/main.dart
+++ flutter_counter_example/lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
