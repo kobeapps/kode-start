9026841dfeaf61a48948424a6d4ed2721c76bd85
Arthur Piccoli <>
2021-11-19 18:48:50 -0300

    add tooltip to button

--- flutter_counter_example/lib/main.dart
+++ flutter_counter_example/lib/main.dart
@@ -56,6 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
+        tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
     );
