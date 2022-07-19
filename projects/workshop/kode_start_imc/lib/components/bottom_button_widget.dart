ec9ccc06ea84314df63a3339a8124e17e771a70b
kobe-thiagofontes <thiago.fontes@kobe.io>
2022-07-19 13:20:52 -0300

    refactor: Divide components and add new behaviors

---
+++ projects/workshop/kode_start_imc/lib/components/bottom_button_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:kode_start_imc/theme/app_colors.dart';
+
+class BottomButtonWidget extends StatelessWidget {
+  const BottomButtonWidget({
+    Key? key,
+    required this.onPressed,
+    required this.title,
+  }) : super(key: key);
+
+  final VoidCallback onPressed;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final paddingBottom = MediaQuery.of(context).padding.bottom;
+    return CupertinoButton(
+      padding: EdgeInsets.zero,
+      onPressed: onPressed,
+      child: Container(
+        height: 65 + paddingBottom,
+        padding: EdgeInsets.only(bottom: paddingBottom),
+        color: AppColors.primaryColorDark,
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              color: AppColors.white,
+              fontSize: 14.5,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- projects/workshop/kode_start_imc/lib/main_page.dart
+++ projects/workshop/kode_start_imc/lib/main_page.dart
@@ -1,268 +1,143 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:kode_start_imc/components/app_bar_component.dart';
+import 'package:kode_start_imc/components/app_bar_widget.dart';
+import 'package:kode_start_imc/components/user_input_widget.dart';
 import 'package:kode_start_imc/result_page.dart';
 import 'package:kode_start_imc/theme/app_colors.dart';
+
+import 'components/bottom_button_widget.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
-  _MainPageState createState() => _MainPageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
   int height = 170;
   int weight = 65;
 
+  Timer? _timer;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBarComponent(context),
+      appBar: AppBarWidget(
+        leftIcon: Icon(
+          Icons.person,
+          color: AppColors.white,
+        ),
+      ),
       body: Container(
         width: MediaQuery.of(context).size.width,
         color: AppColors.black,
         child: Column(
           children: [
-            SizedBox(height: 30),
-            _buildHeightComponent(),
-            SizedBox(height: 20),
-            _buildWeightComponent(),
-            Spacer(),
-            _buildCalculateButton(),
+            const SizedBox(height: 30),
+            UserInputWidget(
+              title: 'Qual a sua altura?',
+              value: height,
+              onIncrease: () {
+                increaseHeight();
+              },
+              onIncreaseHold: () {
+                _timer = Timer.periodic(
+                  const Duration(milliseconds: 200),
+                  (timer) {
+                    increaseHeight();
+                  },
+                );
+              },
+              onIncreaseRelease: () => _timer?.cancel(),
+              onDecrease: () {
+                decreaseHeight();
+              },
+              onDecreaseHold: () {
+                _timer = Timer.periodic(
+                  const Duration(milliseconds: 200),
+                  (timer) {
+                    decreaseHeight();
+                  },
+                );
+              },
+              onDecreaseRelease: () => _timer?.cancel(),
+              measureUnit: 'cm',
+            ),
+            const SizedBox(height: 20),
+            UserInputWidget(
+              height: 140,
+              title: 'Qual o seu peso?',
+              value: weight,
+              onIncrease: () {
+                increaseWeight();
+              },
+              onIncreaseHold: () {
+                _timer = Timer.periodic(
+                  const Duration(milliseconds: 200),
+                  (timer) {
+                    increaseWeight();
+                  },
+                );
+              },
+              onIncreaseRelease: () => _timer?.cancel(),
+              onDecrease: () {
+                decreaseWeight();
+              },
+              onDecreaseHold: () {
+                _timer = Timer.periodic(
+                  const Duration(milliseconds: 200),
+                  (timer) {
+                    decreaseWeight();
+                  },
+                );
+              },
+              onDecreaseRelease: () => _timer?.cancel(),
+              measureUnit: 'kg',
+            ),
+            const Spacer(),
+            BottomButtonWidget(
+              title: 'CALCULAR O RESULTADO',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ResultPage(
+                      weight: weight,
+                      height: height,
+                    ),
+                  ),
+                );
+              },
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildHeightComponent() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Qual a sua altura?",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 14,
-              fontWeight: FontWeight.w900,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10),
-            child: Container(
-              height: 170,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: AppColors.primaryColorLight,
-              ),
-              child: Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      if (height > 0) {
-                        setState(() {
-                          height--;
-                        });
-                      }
-                    },
-                    child: Container(
-                      width: 48,
-                      height: 48,
-                      margin: EdgeInsets.only(left: 21),
-                      decoration: BoxDecoration(
-                        color: AppColors.primaryColorDark,
-                        shape: BoxShape.circle,
-                      ),
-                      child: Icon(
-                        Icons.remove,
-                        color: AppColors.white,
-                        size: 42,
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        height.toString(),
-                        style: TextStyle(
-                          color: AppColors.white,
-                          fontSize: 50,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
-                      Text(
-                        "cm",
-                        style: TextStyle(
-                          color: AppColors.white,
-                          fontSize: 14.5,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        height++;
-                      });
-                    },
-                    child: Container(
-                      width: 48,
-                      height: 48,
-                      margin: EdgeInsets.only(right: 21),
-                      decoration: BoxDecoration(
-                        color: AppColors.primaryColorDark,
-                        shape: BoxShape.circle,
-                      ),
-                      child: Icon(
-                        Icons.add,
-                        color: AppColors.white,
-                        size: 42,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+  void increaseHeight() {
+    return setState(() {
+      height++;
+    });
   }
 
-  Widget _buildWeightComponent() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Qual o seu peso?",
-            style: TextStyle(
-              color: AppColors.white,
-              fontSize: 14,
-              fontWeight: FontWeight.w900,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10),
-            child: Container(
-              height: 170,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: AppColors.primaryColorLight,
-              ),
-              child: Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      if (weight > 0) {
-                        setState(() {
-                          weight--;
-                        });
-                      }
-                    },
-                    child: Container(
-                      width: 48,
-                      height: 48,
-                      margin: EdgeInsets.only(left: 21),
-                      decoration: BoxDecoration(
-                        color: AppColors.primaryColorDark,
-                        shape: BoxShape.circle,
-                      ),
-                      child: Icon(
-                        Icons.remove,
-                        color: AppColors.white,
-                        size: 42,
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        weight.toString(),
-                        style: TextStyle(
-                          color: AppColors.white,
-                          fontSize: 50,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
-                      Text(
-                        "kg",
-                        style: TextStyle(
-                          color: AppColors.white,
-                          fontSize: 14.5,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        weight++;
-                      });
-                    },
-                    child: Container(
-                      width: 48,
-                      height: 48,
-                      margin: EdgeInsets.only(right: 21),
-                      decoration: BoxDecoration(
-                        color: AppColors.primaryColorDark,
-                        shape: BoxShape.circle,
-                      ),
-                      child: Icon(
-                        Icons.add,
-                        color: AppColors.white,
-                        size: 42,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+  void decreaseHeight() {
+    return setState(() {
+      height--;
+    });
   }
 
-  Widget _buildCalculateButton() {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ResultPage(
-              weight: weight,
-              height: height,
-            ),
-          ),
-        );
-      },
-      child: Container(
-        height: 65 + MediaQuery.of(context).padding.bottom,
-        color: AppColors.primaryColorDark,
-        child: Center(
-          child: Text(
-            "CALCULAR O RESULTADO",
-            style: TextStyle(
-              color: AppColors.white,
-              fontSize: 14.5,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
-      ),
-    );
+  void increaseWeight() {
+    return setState(() {
+      weight++;
+    });
+  }
+
+  void decreaseWeight() {
+    return setState(() {
+      weight--;
+    });
   }
 }
